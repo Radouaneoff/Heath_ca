0a03ddafa00441efe414aedd1113a666c070908d
Youcefboudia <youcef.boudia.347@gmail.com>
2024-05-31 20:26:43 +0100

    connection_

--- Api/connection.js
+++ Api/connection.js
@@ -1,13 +1,19 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
 
 const pool = new Pool({
-  user: 'postgres',        
-  host: 'localhost',            
-  database: 'postgres', 
-  password: 'rB2j',    
-  port: 5432,                   
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 pool.connect((err, client, done) => {
   if (err) {
     console.error('Error connecting to the database', err.stack);
@@ -16,5 +22,7 @@ pool.connect((err, client, done) => {
     done();
   }
 });
+
+
 
 module.exports = pool;
